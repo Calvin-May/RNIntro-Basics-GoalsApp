83ee22fc6a98cb45da14f2ea0f5e7243531ce104
Calvin May <calvinkylemay.dev@gmail.com>
2025-03-25 09:38:36 -0400

    Updated general layout

--- App.js
+++ App.js
@@ -9,11 +9,11 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View id='viewGoalInput'>
-        <TextInput placeholder='Your Course Goal' />
+      <View id='viewGoalInput' style={styles.inputContainer}>
+        <TextInput placeholder='Your Course Goal'  style={styles.TextInput}/>
         <Button title='Add Goal' />
       </View>
-      <View id='viewGoalList'>
+      <View id='viewGoalsContainer' style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -23,6 +23,28 @@ export default function App() {
 // Style object that will be used to style our App Component
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 45
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 30,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  TextInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 12,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 6,
+    alignItems: 'center'
   }
 });
