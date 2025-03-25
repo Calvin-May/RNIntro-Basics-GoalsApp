3001b5050848136b9c95dcf375518d82025d501c
Calvin May <calvinkylemay.dev@gmail.com>
2025-03-25 08:34:39 -0400

    Added Comments

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
+// Root Component of the App!
+
+// Import Statements to utilize components provided by default through Expo and React Native packages
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Default functional Component that will render our App!
 export default function App() {
   return (
     <View style={styles.container}>
@@ -10,6 +14,7 @@ export default function App() {
   );
 }
 
+// Style object that will be used to style our App Component
 const styles = StyleSheet.create({
   container: {
     flex: 1,
