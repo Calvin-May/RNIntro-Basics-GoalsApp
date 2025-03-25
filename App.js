3e4bb553e3b80408767f7400429838e761129b1b
Calvin May <calvinkylemay.dev@gmail.com>
2025-03-25 12:06:43 -0400

    Added state to manage a list of goals, goals are now displayed to the user

--- App.js
+++ App.js
@@ -3,21 +3,30 @@
 // Import Statements to utilize components provided by default through Expo and React Native packages
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
-import { useState } from "react";
+import { useState } from "react"; // Allows us to manage simple state
 
 // Default functional Component that will render our App! This simple function returns JSX code that will be
 //-compiled into native Javascript.
 export default function App() {
-  // Variable to hold User Input
+  // Variables to hold User Input and manage state 
   const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
 
   // Function to handle keystrokes on the Goal TextInput component
   function goalInputHandler(enteredText) {
     setEnteredGoalText(enteredText);
   }
 
+  // Function to handle onPress event of button. This Updates the Adds the new Goal to our list of goals
   function addGoalHandler() {
-    console.log(enteredGoalText)
+    // Update the State of the Course Goals, using the text entered by the user
+    //setCourseGoals([...courseGoals, enteredGoalText]);// Not the best way to insert a new item in the array, as our State depends on the previous State of the array
+    setCourseGoals((currentCourseGoals) => (
+      [...currentCourseGoals, enteredGoalText]
+    )); // Best Practice way to update state of an array - Functionally, this appends the new item to the array
+
+    // Same as above, but condensed
+    //setCourseGoals((currentCourseGoals) => [...currentCourseGoals, enteredGoalText]);
   }
   
   return (
@@ -31,7 +40,14 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View id="viewGoalsContainer" style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+      {/*Standard Javascript to loop through the Goal Array, and output JSX that displays all of the goals added by the User */}
+        {courseGoals.map((goal) => (
+          <View key={goal} style={styles.goalItem}>
+            <Text style={styles.goalItemText}>
+              {goal}
+            </Text>
+          </View>
+          ))}
       </View>
     </View>
   );
@@ -62,6 +78,16 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 6,
-    alignItems: "center",
   },
+  goalItem: {
+    margin: 8,
+    padding: 12,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+    
+  },
+  goalItemText: {
+    color: '#ffffffff',
+    fontSize: 20
+  }
 });
