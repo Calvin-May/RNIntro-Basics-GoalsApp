242a64ec2d5e04637d9300889e64433b0ec98224
Calvin May <calvinkylemay.dev@gmail.com>
2025-03-25 09:06:51 -0400

    Added basic components and layout for goal input and display

--- App.js
+++ App.js
@@ -2,31 +2,27 @@
 
 // Import Statements to utilize components provided by default through Expo and React Native packages
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 // Default functional Component that will render our App! This simple function returns JSX code that will be
 //-compiled into native Javascript.
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-      <Button title="I am a Button" />
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View id='viewGoalInput'>
+        <TextInput placeholder='Your Course Goal' />
+        <Button title='Add Goal' />
+      </View>
+      <View id='viewGoalList'>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 // Style object that will be used to style our App Component
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    margin: 16, 
-    borderWidth: 1, 
-    padding: 5
+  appContainer: {
+    padding: 45
   }
 });
