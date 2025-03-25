eb76b5db2f1139108b26d866bbcbe18206146eb1
Calvin May <calvinkylemay.dev@gmail.com>
2025-03-25 13:59:38 -0400

    Added Styling to Goal Items when Pressed to delete

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -3,13 +3,17 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 // Goal Item with Text, onDeleteGoal, and Id Properties passed from App Component
 function GoalItem({ text, onDeleteGoal, id }) {
   return (
-    // Wrap everything within the Pressable component to make the GoalItem Interactable!
-    <Pressable onPress={onDeleteGoal.bind(this, id)}>
-      <View style={styles.goalItem}>
-      {/* Use the Text prop to passed to our Component to display the Goal Text */}
+    <View style={styles.goalItem}>
+      {/*Wrap everything within the Pressable component to make the GoalItem Interactable! */}
+      <Pressable 
+        onPress={onDeleteGoal.bind(this, id)} 
+        android_ripple={styles.androidRipple} // Android Ripple will work on Android but not IOS
+        style={({pressed}) => pressed && styles.pressedItemIOS} // Alternative Styling for IOS using Pressed
+      > 
+        {/* Use the Text prop to passed to our Component to display the Goal Text */}
         <Text style={styles.goalItemText}>{text}</Text> 
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
@@ -18,14 +22,20 @@ export default GoalItem;
 const styles = StyleSheet.create({
     goalItem: {
     margin: 8,
-    padding: 12,
     borderRadius: 6,
     backgroundColor: '#5e0acc',
   },
   goalItemText: {
     color: '#ffffffff',
-    fontSize: 20
-  }
+    fontSize: 20,
+    padding: 12,
+  },
+  androidRipple : { // Used for Android Devices
+    color: '#39087a',
+  },
+  pressedItemIOS: { // Used for IOS Devices
+    opacity: 0.5,
+  },
 });
 
 
